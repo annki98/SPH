7f78a60f6b16560cf996b878d6765aa949be7fe4
mbreibach <breibach8@gmail.com>
2021-11-30 14:22:48 +0100

    Add Mono-behavior SPH from video

---
+++ Assets/Scripts/ParticleCollider.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleCollider : MonoBehaviour
+{
+    public Vector3 Position;
+    public Vector3 Up;
+    public Vector3 Right;
+    public Vector2 Scale;
+
+}
